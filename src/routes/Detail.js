74c36365baeab2eadc7523b72611fa7013d9b62b
Jinu <pjw019472@gmail.com>
2022-05-17 21:29:49 +0900

    movie fetching system

---
+++ src/routes/Detail.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movieInfo, setMovieInfo] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovieInfo(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>
+            {movieInfo.title} ({movieInfo.year})
+          </h1>
+          <img src={movieInfo.large_cover_image} alt={movieInfo.title} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Detail;
